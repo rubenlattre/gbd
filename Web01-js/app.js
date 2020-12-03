c86b292bd8b14c47b2a68132b38b09efaf6f0d3d
rubenlattre <72036058+rubenlattre@users.noreply.github.com>
2020-12-03 16:42:18 +0100

    Add files via upload

---
+++ Web01-js/app.js
@@ -0,0 +1,22 @@
+document.write ("Hola mundo")
+
+
+let texto = "Esto es un texto";
+let numero = 25;
+let decimal = 5.4;
+let booleano = true ;
+
+let array = ["Jose", 58, "moreno"];
+
+let persona = {
+    "nombre" : "manuel",
+    "edad" : 23,
+    "cabello" : "rubio"
+}
+console.log(texto);
+console.log(numero);
+console.log(decimal);
+console.log(booleano);
+console.log(array);
+console.log(persona["nombre"]);
+console.log(array[1])
